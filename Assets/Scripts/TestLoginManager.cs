48ef250b757cbff0a1e3cf3dc9efacbc4823e2d6
Zee Vasilyev <zeejfps@gmail.com>
2023-03-22 19:04:39 -0500

    Removed namespace

--- Assets/Scripts/TestLoginManager.cs
+++ Assets/Scripts/TestLoginManager.cs
@@ -1,73 +1,71 @@
 using System;
 using System.Threading.Tasks;
+using Login;
 using UnityEngine;
 using YADBF;
 
-namespace Login
+internal sealed class TestLoginManager : ILoginManager
 {
-    internal sealed class TestLoginManager : ILoginManager
+    public ObservableProperty<string> EmailProp { get; } = new();
+    public ObservableProperty<string> PasswordProp { get; } = new();
+    public ObservableProperty<bool> IsLoadingProp { get; } = new();
+    public ObservableProperty<Action> LoginActionProp { get; } = new();
+
+    private IPopupService PopupService { get; }
+
+    public TestLoginManager(IPopupService popupService)
     {
-        public ObservableProperty<string> EmailProp { get; } = new();
-        public ObservableProperty<string> PasswordProp { get; } = new();
-        public ObservableProperty<bool> IsLoadingProp { get; } = new();
-        public ObservableProperty<Action> LoginActionProp { get; } = new();
+        PopupService = popupService;
+        EmailProp.ValueChanged += EmailProp_OnValueChanged;
+        PasswordProp.ValueChanged += PasswordProp_OnValueChanged;
+        UpdateState();
+    }
 
-        private IPopupService PopupService { get; }
+    private void EmailProp_OnValueChanged(ObservableProperty<string> property, string prevvalue, string currvalue)
+    {
+        UpdateState();
+    }
 
-        public TestLoginManager(IPopupService popupService)
+    private void PasswordProp_OnValueChanged(ObservableProperty<string> property, string prevvalue, string currvalue)
+    {
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        var email = EmailProp.Value;
+        var password = PasswordProp.Value;
+
+        if (string.IsNullOrEmpty(email) ||
+            string.IsNullOrEmpty(password))
         {
-            PopupService = popupService;
-            EmailProp.ValueChanged += EmailProp_OnValueChanged;
-            PasswordProp.ValueChanged += PasswordProp_OnValueChanged;
-            UpdateState();
+            LoginActionProp.Set(null);
         }
-
-        private void EmailProp_OnValueChanged(ObservableProperty<string> property, string prevvalue, string currvalue)
+        else
         {
-            UpdateState();
+            LoginActionProp.Set(LoginAsync);
         }
+    }
 
-        private void PasswordProp_OnValueChanged(ObservableProperty<string> property, string prevvalue, string currvalue)
-        {
-            UpdateState();
-        }
-
-        private void UpdateState()
+    private async void LoginAsync()
+    {
+        try
         {
             var email = EmailProp.Value;
             var password = PasswordProp.Value;
 
-            if (string.IsNullOrEmpty(email) ||
-                string.IsNullOrEmpty(password))
-            {
-                LoginActionProp.Set(null);
-            }
-            else
-            {
-                LoginActionProp.Set(LoginAsync);
-            }
+            IsLoadingProp.Set(true);
+
+            await Task.Delay(2000);
+            await PopupService.ShowInfoPopupAsync("Invalid Credentials", "Email and/or Password was incorrect");
         }
-
-        private async void LoginAsync()
+        catch (Exception e)
         {
-            try
-            {
-                var email = EmailProp.Value;
-                var password = PasswordProp.Value;
-
-                IsLoadingProp.Set(true);
-
-                await Task.Delay(2000);
-                await PopupService.ShowInfoPopupAsync("Invalid Credentials", "Email and/or Password was incorrect");
-            }
-            catch (Exception e)
-            {
-
-            }
-            finally
-            {
-                IsLoadingProp.Set(false);
-            }
+            Debug.LogException(e);
+        }
+        finally
+        {
+            IsLoadingProp.Set(false);
         }
     }
 }
