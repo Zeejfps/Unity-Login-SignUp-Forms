7fae1052cfdb903c5c0e684e6e883aab1c8d0ef5
Zee Vasilyev <zeejfps@gmail.com>
2023-03-24 15:43:32 -0500

    Cleaned up LoginWidgetView

--- Assets/Scripts/UGUILoginFormWidgetView.cs
+++ Assets/Scripts/UGUILoginFormWidgetView.cs
@@ -1,22 +1,18 @@
+using Login;
 using UnityEngine;
 using YADBF.Unity;
 
-namespace Login
+public sealed class UGUILoginFormWidgetView : UGUIWidgetView<ILoginFormWidget>
 {
-    public sealed class UGUILoginFormWidgetView : UGUIWidgetView<ILoginFormWidget>
+    [SerializeField] private View<ITextInputWidget> m_EmailInputWidgetView;
+    [SerializeField] private View<IPasswordFieldWidget> m_PasswordInputWidgetView;
+    [SerializeField] private View<IButtonWidget> m_LoginButtonWidgetView;
+
+    protected override void OnBindToModel(ILoginFormWidget model)
     {
-        [SerializeField] private View<ITextInputWidget> m_EmailInputWidgetView;
-        [SerializeField] private UGUIPasswordFieldWidgetView m_PasswordInputWidgetView;
-        [SerializeField] private UGUIButtonWidgetView m_LoginButtonWidgetView;
-
-        protected override void OnBindToModel(ILoginFormWidget model)
-        {
-            base.OnBindToModel(model);
-            Bind(model.IsVisibleProp, gameObject.SetActive);
-
-            m_EmailInputWidgetView.Model = model.EmailInputWidget;
-            m_PasswordInputWidgetView.Model = model.PasswordInputWidget;
-            m_LoginButtonWidgetView.Model = model.LoginButtonWidget;
-        }
+        base.OnBindToModel(model);
+        m_EmailInputWidgetView.Model = model.EmailInputWidget;
+        m_PasswordInputWidgetView.Model = model.PasswordInputWidget;
+        m_LoginButtonWidgetView.Model = model.LoginButtonWidget;
     }
 }
