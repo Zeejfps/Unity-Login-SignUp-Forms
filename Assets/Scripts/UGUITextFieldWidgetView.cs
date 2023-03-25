ca6412d7d58b21909167b29a5a2689943be413dd
Zee Vasilyev <zeejfps@gmail.com>
2023-03-25 15:44:20 -0500

    Added a highlight

--- Assets/Scripts/UGUITextFieldWidgetView.cs
+++ Assets/Scripts/UGUITextFieldWidgetView.cs
@@ -7,6 +7,7 @@ public sealed class UGUITextFieldWidgetView : UGUIWidgetView<ITextFieldWidget>
 {
     [SerializeField] private View<ITextInputWidget> m_TextInputWidgetView;
     [SerializeField] private TMP_Text m_ErrorText;
+    [SerializeField] private GameObject m_ErrorHighlight;
 
     protected override void OnBindToModel(ITextFieldWidget model)
     {
@@ -14,7 +15,9 @@ public sealed class UGUITextFieldWidgetView : UGUIWidgetView<ITextFieldWidget>
         m_TextInputWidgetView.Model = model.TextInputWidget;
         Bind(model.ErrorTextProp, value =>
         {
+            var showHighlight = !string.IsNullOrWhiteSpace(value);
             m_ErrorText.text = value;
+            m_ErrorHighlight.SetActive(showHighlight);
         });
     }
 }
