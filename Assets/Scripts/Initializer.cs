d0552724eb321beae8fccb78623ee9bfbd2563f8
Zee Vasilyev <zeejfps@gmail.com>
2023-03-25 12:07:26 -0500

    Changed stuff from singletons to scoped

--- Assets/Scripts/Initializer.cs
+++ Assets/Scripts/Initializer.cs
@@ -6,13 +6,11 @@ public class Initializer : MonoBehaviour
 {
     private void Awake()
     {
-        Z.RegisterSingleton<ILoginForm, TestLoginForm>();
         Z.RegisterSingleton<IPopupManager, PopupManager>();
-        Z.RegisterSingleton<ISignUpForm, TestSignUpForm>();
-        Z.RegisterSingleton<ISignUpConfirmationForm, TestSignUpConfirmationForm>();
         
-        Z.RegisterScoped<ILoginFormWidget, LoginFormWidget>();
-        Z.RegisterScoped<ISignUpFormWidget, SignUpFormWidget>();
-        Z.RegisterScoped< ILoginSignUpWidget, LoginSignUpWidget>();
+        Z.RegisterScoped<ILoginForm, TestLoginForm>();
+        Z.RegisterScoped<ISignUpForm, TestSignUpForm>();
+        Z.RegisterScoped<ISignUpConfirmationForm, TestSignUpConfirmationForm>();
+        Z.RegisterScoped<ILoginSignUpWidget, LoginSignUpWidget>();
     }
 }
