50fcf2a021d50ca555419b238c2ad858ac70e672
Zee Vasilyev <zeejfps@gmail.com>
2023-03-25 12:15:57 -0500

    Preparing to add validation

--- Assets/Scripts/SignUpFormWidget.cs
+++ Assets/Scripts/SignUpFormWidget.cs
@@ -1,28 +1,25 @@
 using YADBF;
 
-namespace Login
+internal sealed class SignUpFormWidget : ISignUpFormWidget
 {
-    internal sealed class SignUpFormWidget : ISignUpFormWidget
-    {
-        public ObservableProperty<bool> IsVisibleProp { get; } = new();
-        public ITextInputWidget EmailInputWidget { get; }
-        public ITextInputWidget UsernameInputWidget { get; }
-        public IPasswordFieldWidget PasswordFieldWidget { get; }
-        public IPasswordFieldWidget ConfirmPasswordFieldWidget { get; }
-        public IButtonWidget SignUpButtonWidget { get; }
+    public ObservableProperty<bool> IsVisibleProp { get; } = new();
+    public ITextInputWidget EmailInputWidget { get; }
+    public ITextInputWidget UsernameInputWidget { get; }
+    public IPasswordFieldWidget PasswordFieldWidget { get; }
+    public IPasswordFieldWidget ConfirmPasswordFieldWidget { get; }
+    public IButtonWidget SignUpButtonWidget { get; }
 
-        public SignUpFormWidget(
-            ITextInputWidget emailInputWidget, 
-            ITextInputWidget usernameInputWidget,
-            IPasswordFieldWidget passwordFieldWidget,
-            IPasswordFieldWidget confirmPasswordFieldWidget,
-            IButtonWidget signUpButtonWidget
-        ) {
-            EmailInputWidget = emailInputWidget;
-            UsernameInputWidget = usernameInputWidget;
-            PasswordFieldWidget = passwordFieldWidget;
-            ConfirmPasswordFieldWidget = confirmPasswordFieldWidget;
-            SignUpButtonWidget = signUpButtonWidget;
-        }
+    public SignUpFormWidget(
+        ITextInputWidget emailInputWidget, 
+        ITextInputWidget usernameInputWidget,
+        IPasswordFieldWidget passwordFieldWidget,
+        IPasswordFieldWidget confirmPasswordFieldWidget,
+        IButtonWidget signUpButtonWidget
+    ) {
+        EmailInputWidget = emailInputWidget;
+        UsernameInputWidget = usernameInputWidget;
+        PasswordFieldWidget = passwordFieldWidget;
+        ConfirmPasswordFieldWidget = confirmPasswordFieldWidget;
+        SignUpButtonWidget = signUpButtonWidget;
     }
 }
