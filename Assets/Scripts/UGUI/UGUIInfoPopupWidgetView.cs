cfc14ec5597250c7ad2abee4157c07997258d798
Zee Vasilyev <zeejfps@gmail.com>
2023-04-06 14:37:47 -0500

    Preparing to cleanup the Info Popup

--- Assets/Scripts/UGUI/UGUIInfoPopupWidgetView.cs
+++ Assets/Scripts/UGUI/UGUIInfoPopupWidgetView.cs
@@ -1,16 +1,29 @@
 using Common.Widgets;
 using Login;
+using TMPro;
 using UnityEngine;
 
 namespace UGUI
 {
     public sealed class UGUIInfoPopupWidgetView : UGUIWidgetView<IInfoPopupWidget>
     {
+        [SerializeField] private TMP_Text m_TitleText;
+        [SerializeField] private TMP_Text m_ContentText;
         [SerializeField] private UGUIButtonWidgetView m_ButtonWidgetView;
     
         protected override void OnBindToModel(IInfoPopupWidget model)
         {
             base.OnBindToModel(model);
+            Bind(model.TitleTextProp, value =>
+            {
+                m_TitleText.SetText(value);
+            });
+
+            Bind(model.InfoTextProp, value =>
+            {
+                m_ContentText.text = value;
+                
+            });
             m_ButtonWidgetView.Model = new ActionPropertyButtonWidget(model.OkActionProp);
         }
     }
